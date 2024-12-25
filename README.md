b344c921faee043da9edbbea64bf55ef54036ab7
lutz.ackermann99@gmail.com <lutz.ackermann@baeckerai.de>
2024-12-25 16:46:57 +0100

    removed decorator test

--- README.md
+++ README.md
@@ -3,8 +3,20 @@
 This repository contains a template for a FastAPI with Keyloak as user management.
 An additional database using postgres is considered.
 
-Create an `.env` file with following content:
+# Usage
+To use this Code apply the following steps:
 
+1. Create a Venv:
+```
+python -m venv <myenvpath>
+```
+
+2. Install requirements:
+```
+pip install -r src/requirements/requirements.txt
+```
+
+3. Create a `.env`-file:
 ```
 DATABASE_URL = postgresql+asyncpg://username:password@host:port
 KEYCLOAK_ADMIN = <admin-username>
@@ -14,4 +26,9 @@ KEYCLOAK_CLIENT_ID=<client-name>
 KEYCLOAK_CLIENT_SECRET=<client-secrect>
 ALGORITHM = RS256
 MODE=<DEV or PROD>
+```
+
+4. Start all docker container:
+```
+docker compose up
 ```
--- src/app/resources/user/users_endpoint.py
+++ src/app/resources/user/users_endpoint.py
@@ -4,7 +4,6 @@ from sqlalchemy.sql import text
 
 from ...database import get_async_db
 from ...user_management.keycloack_custom import verify_token
-from ...shared.decorators import auth_required
 
 router = APIRouter()
 
@@ -20,27 +19,6 @@ async def get_user(
             {
                 "id": user.id,
                 "username": user.username,
-                "email": user.email,
-                "password_hash": user.password_hash,
-            }
-        )
-    return users
-
-
-@router.get("/decorator")
-@auth_required
-# This function validates the token using the auth_required decorator
-# to avoid unnecessary code repetition and unused parameters
-async def get_user(request: Request, db: AsyncSession = Depends(get_async_db)):
-    result = await db.execute(text("Select * from users;"))
-    users = []
-    for user in result.all():
-        users.append(
-            {
-                "id": user.id,
-                "username": user.username,
-                "email": user.email,
-                "password_hash": user.password_hash,
             }
         )
     return users
